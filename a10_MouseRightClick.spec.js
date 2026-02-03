96d5b3ad11c4afcb1cc83e82c966b008792f31be
Sneha456244 <151616854+Sneha456244@users.noreply.github.com>
2026-02-03 17:44:00 +0530

    Create a10_MouseRightClick.spec.js

---
+++ a10_MouseRightClick.spec.js
@@ -0,0 +1,13 @@
+const {test,expect}=require('@playwright/test')
+
+test ('Mouse Right Click ',async({page})=>{
+
+    await page.goto('http://swisnl.github.io/jQuery-contextMenu/demo.html')
+
+    const button=await page.locator("//span[@class='context-menu-one btn btn-neutral']")
+
+    //Right click action
+    await button.click({button:'right'});
+
+    await page.waitForTimeout(4000)
+})
